4ec96f6f591ffa2df427d28baeb64791c1f54a6a
Manav Nenwani <manavnenwani2810@gmail.com>
2026-01-28 21:27:59 +0530

    Update urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("about/", views.about, name="about"),
-
-    path("add/", views.add_topic, name="add_topic"),
-    path("progress/", views.view_progress, name="view_progress"),
-
-    path("update/<int:topic_id>/", views.update_topic, name="update_topic"),
-    path("delete/<int:topic_id>/", views.delete_topic, name="delete_topic"),
+    path("", views.dashboard, name="dashboard"),
+    path("topic/add/", views.add_topic, name="add_topic"),
+    path("topic/<int:topic_id>/edit/", views.update_topic, name="update_topic"),
+    path("topic/<int:topic_id>/delete/", views.delete_topic, name="delete_topic"),
+    path("topic/<int:topic_id>/progress/", views.add_progress, name="add_progress"),
 ]
